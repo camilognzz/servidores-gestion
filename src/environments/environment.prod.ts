cf78490abeeb23d140122530ab6aa3a43e96e5cd
Camilo Gonzalez <camilogs2218@gmail.com>
2025-03-12 09:45:00 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyC7iWD95T_k4LJDHCeIy_84D3HljL6Aj54",
+      authDomain: "servidores-gestion.firebaseapp.com",
+      projectId: "servidores-gestion",
+      storageBucket: "servidores-gestion.firebasestorage.app",
+      messagingSenderId: "1036733007979",
+      appId: "1:1036733007979:web:131c43ff3e7666cd1ac92e"
+    }
+  };
+  
